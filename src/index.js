537fb2b73d186d58b140238e09f7d8b2ca69bf45
lokechan24 <gklokeshwaran@gmail.com>
2024-02-07 14:49:38 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter as Router } from "react-router-dom"
+
+const root = document.getElementById('root'); // Corrected syntax for selecting the root element
+const rootElement = root ? ReactDOM.createRoot(root) : null; // Creating a root if 'root' element is found
+if (rootElement) {
+  rootElement.render(
+    <React.StrictMode>
+      <Router>
+        <App />
+      </Router>
+    </React.StrictMode>
+  );
+}
