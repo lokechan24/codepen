f4b19ef20ce3b56db083f4a65a16a25f7db12974
lokechan24 <gklokeshwaran@gmail.com>
2024-02-09 15:59:21 +0530

    9th feb eveing 4pm

--- src/container/Home.jsx
+++ src/container/Home.jsx
@@ -1,9 +1,12 @@
 import React, { useState } from "react";
 import { HiChevronDoubleLeft } from "react-icons/hi2";
 import { MdHome } from "react-icons/md";
+import { FaSearchengin } from "react-icons/fa6";
 import { motion } from "framer-motion";
-import { Link } from "react-router-dom";
+import { Link, Route, Routes } from "react-router-dom";
 import { Logo } from "../assets";
+import Projects from "./Projects";
+import SignUp from "./SignUp";
 
 const Home = () => {
     const [isSideMenu, setSideMenu] = useState(false);
@@ -44,6 +47,38 @@ const Home = () => {
         </div>
         <div className="flex-1 min-h-screen max-h-screen overflow-y-scroll h-full flex flex-col items-start justify-start px-4 md:px12 py-4 md:py-12">
             {/* top section */}
+            <div className="w-full flex items-center justify-between gap-3">
+                {/* serach section */}
+                <div className="bg-secondary w-full px-4 py-3 rounded-md flex items-center justify-center gap-3">
+                    <FaSearchengin className="text-2xl text-primaryText" />
+                    <input
+                        type="text"
+                        className="flex-1 px-4 py-1 text-xl bg-transparent outline-none border-none text-primaryText placeholder:text-gray-600 capitalize" placeholder="search here..." />
+                </div>
+                {/* profil section */}
+                {!user && (
+                    <motion.div
+                        whileTap={{ scale: 0.9 }}
+                        className="flex items-center justify-center gap-3"
+                    >
+                        <Link
+                            to={"/home/auth"}
+                            className="bg-emerald-500 px-6 py-2 rounded-md text-white text-lg cursor-pointer hover:bg-emerald-700 capitalize"
+                        >
+                            sign up
+                        </Link>
+                    </motion.div>
+                )}
+{!user && <div></div>}
+
+            </div>
+            {/* bottom section */}
+            <div className="w-full">
+                <Routes>
+                    <Route path="/*" element={<Projects/>}/>
+                    <Route path="/auth" element={<SignUp/>}/>
+                </Routes>
+            </div>
         </div>
     </>
 };
---
+++ src/container/Projects.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Projects = () => {
+    return (
+    <div className="text-primaryText">Project</div>
+    )
+}
+
+export default Projects;
